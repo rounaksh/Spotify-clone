b238838a2e9d91f0c6911b68f9092a33acb9f469
rounak sharma <rounak.0734@gmail.com>
2024-12-09 10:52:27 +0530

    Implimented socket.io in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import { clerkMiddleware } from '@clerk/express'
 import fileUpload from 'express-fileupload'
 import path from 'path'
 import cors from 'cors'
+import { initializeSocket } from './lib/socket.js'
 
 import { connectDB } from './lib/db.js'
 import userRoutes from './routes/user.route.js'
@@ -12,12 +13,16 @@ import authRoutes from './routes/auth.route.js'
 import songsRoutes from './routes/song.route.js'
 import albumsRoutes from './routes/album.route.js'
 import statsRoutes from './routes/stats.route.js'
+import { createServer } from 'http'
 
 dotenv.config()
 
 const __dirname = path.resolve()
 const app = express()
 const PORT = process.env.PORT
+
+const httpServer = createServer(app)
+initializeSocket(httpServer)
 
 app.use(cors({
     origin: 'http://localhost:3000',
@@ -46,9 +51,8 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: process.env.NODE_ENV === 'production' ? "Internal server error" : err.message })
 })
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log("Server is running on port ", PORT)
     connectDB()
 })
 
-// todo: socket.io
